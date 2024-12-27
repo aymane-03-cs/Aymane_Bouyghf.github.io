028720a0a74954e3c9c8c84b74f7a13a2c7a70c7
Aymane Bouyghf <aymane.bouyghf@enseirb-matmeca.fr>
2024-12-27 13:53:51 +0100

    Home path adapted

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <Header />
       <main style={{ flex: 1 }}>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/Home" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/About" element={<About />} />
           <Route path="/Contact" element={<Contact />} />
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Aymane_Bouyghf.github.io/', // Chemin de base pour un site personnel (username.github.io)
+  base: '/Aymane_Bouyghf.github.io/Home', // Chemin de base pour un site personnel (username.github.io)
 });
