4e1c05fb46ee23be65c031f5f13115952a6cf202
Aymane Bouyghf <aymane.bouyghf@enseirb-matmeca.fr>
2024-12-27 13:40:13 +0100

    vite.confing: base path modified

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/aymane-03-cs.github.io/', // Chemin de base pour un site personnel (username.github.io)
+  base: '/Aymane_Bouyghf.github.io/', // Chemin de base pour un site personnel (username.github.io)
 });
